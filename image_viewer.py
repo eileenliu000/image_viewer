d5f6e24998eeacc526fe7a2edf53478bc59e5cfa
eileenliu000 <103693741+eileenliu000@users.noreply.github.com>
2022-04-21 16:34:40 -0700

    Update image_viewer.py

--- image_viewer.py
+++ image_viewer.py
@@ -1,5 +1,6 @@
+import random
 from tkinter import *
-from PIL import Image, ImageTk
+from PIL import ImageTk, Image
 
 root = Tk()
 root.title("Image Viewer")
@@ -12,9 +13,8 @@ my_img5 = ImageTk.PhotoImage(Image.open("picture5.png"))
 
 image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 
-my_label = Label(image=my_img)
+my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
-
 
 def forward(image_number):
     global my_label
@@ -22,17 +22,16 @@ def forward(image_number):
     global button_back
 
     my_label.grid_forget()
-    my_label = Label(image=image_list[image_number-1])
-    button_forward = Button(root, text='>>', command=lambda: forward(image_number+1))
-    button_back = Button(root, text='<<', command=lambda: back(image_number-1))
+    my_label=Label(image=image_list[image_number-1])
+    button_forward= Button(root, text='>>', command=lambda: forward(image_number+1))
+    button_back=Button(root, text='<<', command=lambda: back(image_number-1))
 
     if image_number==5:
-        button_forward = Button(root, text='>>', state=DISABLED)
+        button_forward= Button(root, text='>>', state=DISABLED)
 
     my_label.grid(row=0, column=0, columnspan=3)
     button_back.grid(row=1,column=0)
     button_forward.grid(row=1, column=2)
-
 
 def back(image_number):
     global my_label
@@ -40,17 +39,16 @@ def back(image_number):
     global button_back
 
     my_label.grid_forget()
-    my_label = Label(image=image_list[image_number + 1])
+    my_label = Label(image=image_list[image_number - 1])
     button_forward = Button(root, text='>>', command=lambda: forward(image_number + 1))
     button_back = Button(root, text='<<', command=lambda: back(image_number - 1))
 
     if image_number==1:
-        button_forward= Button(root, text='>>', state=DISABLED)
+        button_back= Button(root, text='<<', state=DISABLED)
 
     my_label.grid(row=0, column=0, columnspan=3)
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
-
 
 button_back = Button(root, text="<<", command=back, state=DISABLED)
 buttonquit = Button(root, text="Exit", command=root.quit)
